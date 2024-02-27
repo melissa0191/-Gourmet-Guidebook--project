da1bf6e31eb44c472f0ebad775500fd467113217
melissavelasquezgreene <melissa0191@outlook.es>
2024-02-27 16:32:40 -0500

    app.py

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to Gourmet Guidebook</h2>
+            <p>This is the home page of the Recipe Manager application. You can use this application to manage your recipes and categories.</p>
+            <p>Get started by navigating to the Recipes or Categories page using the navigation links above.</p>
+        </div>
+    );
+};
+
+export default Home;
